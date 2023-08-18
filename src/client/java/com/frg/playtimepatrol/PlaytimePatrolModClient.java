154b284880e9049235709c8ab944b1df1e53e201
Jean Farrugia <jeanfrg@gmail.com>
2023-08-18 16:12:14 +0200

    chore: renamed main classes

--- src/client/java/com/frg/playtimepatrol/PlaytimePatrolModClient.java
+++ src/client/java/com/frg/playtimepatrol/PlaytimePatrolModClient.java
@@ -2,7 +2,7 @@ package com.frg.playtimepatrol;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class PlaytimePatrolModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/main/java/com/frg/playtimepatrol/PlaytimePatrolMod.java
+++ src/main/java/com/frg/playtimepatrol/PlaytimePatrolMod.java
@@ -13,7 +13,7 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.UUID;
 
-public class PlaytimePatrol implements ModInitializer {
+public class PlaytimePatrolMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
