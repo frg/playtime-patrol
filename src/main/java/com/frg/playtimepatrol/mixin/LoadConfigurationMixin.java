5c8fb67e8605e32d4f76984a5830af9e9285a653
Jean Farrugia <jeanfrg@gmail.com>
2023-08-20 21:05:48 +0200

    feat: Added autoconfig integration, loading config

--- src/main/java/com/frg/playtimepatrol/mixin/LoadConfigurationMixin.java
+++ src/main/java/com/frg/playtimepatrol/mixin/LoadConfigurationMixin.java
@@ -1,5 +1,9 @@
 package com.frg.playtimepatrol.mixin;
 
+import com.frg.playtimepatrol.ModConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.ConfigHolder;
+import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -7,9 +11,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
-public class ExampleMixin {
+public class LoadConfigurationMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+		// This code is injected into the start of MinecraftServer.loadWorld()
+		ConfigHolder<ModConfig> holder = AutoConfig.register(
+				ModConfig.class,
+				Toml4jConfigSerializer::new
+		);
 	}
 }
