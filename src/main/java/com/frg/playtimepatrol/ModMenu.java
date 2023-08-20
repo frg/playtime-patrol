c28954a9d6f0df9ff4fd9fedce0ca82c3b6e1985
Jean Farrugia <jeanfrg@gmail.com>
2023-08-20 21:06:15 +0200

    feat: Created mod menu integration

---
+++ src/main/java/com/frg/playtimepatrol/ModMenu.java
@@ -0,0 +1,16 @@
+package com.frg.playtimepatrol;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@SuppressWarnings("unused") // entrypoint
+@Environment(EnvType.CLIENT)
+public class ModMenu implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return screen -> AutoConfig.getConfigScreen(ModConfig.class, screen).get();
+    }
+}
